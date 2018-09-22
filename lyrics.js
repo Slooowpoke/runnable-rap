6cdb285b6cf84ffa8b324b494e31e1427bb4caf2
Mali Coxon <malii.coxon@gmail.com>
2018-09-22 22:55:14 +0100

    Create lyrics.js

---
+++ lyrics.js
@@ -0,0 +1,107 @@
+// Grab ya window and hit f12
+// Click the console, get ready to delve,
+
+// First we start off with something simple and dashing
+const woah = /* ah */" ", brap = /* slash-n */"\n"; 
+const cuticle = /* is */ 16;
+// Keep the vars nice and mean
+
+const bling = /*blank*/"";
+/* hope it'll make bank */
+/* who'am kidding, this shit stank.. */
+
+// Can't work this bit in
+const bam = /* whatt */"&#9642;"
+const mmm = /* yup*/0;
+// No need for a number seven
+const trulyIsHeaven = 500;
+
+
+// Gotta write some utils now
+function aintNeverBeenFruityFoul(throwInTheHex) {
+  return (parseInt(
+  throwInTheHex, /* cut your */ cuticle /*in*/).
+  /* don't be a vexin */
+  /* hex to bin gotta return a */
+  toString(2))
+  /* pad the start to really hear it sing */
+  .padStart(cuticle /*divided by */ /  2, /* don't need no pharmaceticuls just */ '0' /* and poom */ );
+}
+
+// That's it, the first conjunction
+// Get ready now here comes the next
+function /*! , */ getLetterBrah(inGoesBin) {
+	inGoesBin = inGoesBin.split(bling);
+  let chunks = Array();
+  // are saviours, now fill with all kinds of flavours
+  [...Array(5).keys()]
+  .map(_ => chunks.push(inGoesBin.slice(_ * 5, 5 + _ * 5)))// oh my my my
+  return chunks;
+}
+
+// We made it through that one, two more utils to go
+function parseBar /*Make you cry oh-woah no*/(beautiful) {
+  return {
+    thereputical: beautiful.charAt(mmm, 1),
+    line: "1" + (beautiful.slice(1, beautiful.length)
+    						.toUpperCase()
+                .split(woah)
+                .reduce((sauce, tomatoes) => 
+                sauce + (!isNaN(tomatoes) ? parseInt(tomatoes) : tomatoes.charAt(mmm))))
+  };
+}
+
+let language = /*object*/{};
+// and time to affect
+// This shit goes in and doesn't make much sense
+// Don't read the first letter, you'll be on the offense
+[
+  "S Fabulous 87 called-at 3 fiteen",
+  "Y 18 foned cause 3 flipped-me",
+  "N Forced 8 churros-at 631",/*feet*/
+  "T Face-me-and-my 21084",/* beats */
+  "A Fished 8 front ends-on-the 31",/*st*/
+  "X 15 11 51",/* what a weird verse */
+  "R Forty 8 friends explored 93",
+  "E Fun 87 eats 1 finger-with-me",
+  "O Forfitted 8663 friends"
+  // Just because my gibber gabber never ends
+].map(parseBar).map(itYar => {
+  language[itYar./*dot*/thereputical] = getLetterBrah((aintNeverBeenFruityFoul(itYar.line)))// Been contrived but hey its fine
+});
+
+var complement = document.createElement("div");
+complement.style = /* just for the sentiment viv */
+"position:absolute; z-index:9000;"
+/* Better than bus route, For your own arousement?*/
+document.body.appendChild(complement);
+// Needs the kids cause papa went
+
+function print(text) {
+  let oh = bling;
+
+  text.split(bling).map((c) => {
+    language[c].map((n) => {
+      n.map((b) => (parseInt(b)) ? oh += bam + woah : oh += woah + woah)
+      oh += brap;
+    });
+    oh += brap;
+  });
+
+  let commandment = document.createElement("pre");
+
+  let style /* is.. what a shock */ = "display:inline-block;"
+  style += "margin-right:20px";// empty and official
+  commandment.style = style;// For a while
+  commandment.innerHTML = oh /* ring the bell */;
+  complement.appendChild(commandment);// Constant improvement
+}
+
+try /* it, it won't be */{
+  eval("bang bang bling cats")
+} catch (fats) {
+  let stacks = (bling + fats).slice(mmm,11).toUpperCase();
+	[...Array(trulyIsHeaven).keys()].map(_ => print(stacks));
+}
+
+
